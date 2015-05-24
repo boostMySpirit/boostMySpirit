3912a2362ea9588992db105b2bcdb657266c7b50
boostMySpirit <boostMySpirit@users.noreply.github.com>
2015-05-24 15:51:06 +0200

    Add Phone number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524123532) do
+ActiveRecord::Schema.define(version: 20150524124601) do
 
   create_table "periodic_events", force: :cascade do |t|
     t.string   "eventType",   limit: 255
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150524123532) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "phone",                  limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
